2d55f793e345816f274865a25425a04a90e4b058
Caelen <caelenmensch@gmail.com>
2022-03-09 18:02:27 -0800

    ready player me package

---
+++ Assets/Plugins/Wolf3D Ready Player Me SDK/Scripts/Utils/OutfitGender.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Wolf3D.ReadyPlayerMe.AvatarSDK
+{
+    public enum OutfitGender
+    {
+        [Description("masculine")]
+        Masculine,
+        [Description("feminine")]
+        Feminine
+    }
+}
